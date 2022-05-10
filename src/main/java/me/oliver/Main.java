fd34817df4ef319b885f148e29d1134123db7bd0
QuartzzX <oliverlongnz@gmail.com>
2022-05-11 08:29:43 +1200

    8.29

---
+++ src/main/java/me/oliver/Main.java
@@ -0,0 +1,45 @@
+package me.oliver;
+//create a function thats asks the user for input and then when selected will display and be assigned to approiate variable.
+
+import me.oliver.data.Data;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        getInput();
+    }
+
+
+    public static void getInput() {
+        Scanner sc = new Scanner(System.in);
+
+
+         if(sc.hasNextBoolean()){
+            System.out.println("bool");
+             //boolean = true/false
+        } else if(sc.hasNextShort()){
+            System.out.print("short");
+             //short numberMin = -32768 numberMax = 32767;
+        } else if(sc.hasNextLong()){
+            System.out.print("Long");
+            //long has a minimum value of -263x10^63 and a maximum value of 263x10^63-1.
+        } else if(sc.hasNextInt()){
+            System.out.println("That is a integer");
+            //integer is any number (if have long and short cannot get integer)
+        } else if(sc.next().equalsIgnoreCase("findArray")) {
+             for(String str : Data.array){
+                 System.out.println(str);
+                 // loops through all items in the array located in data file and prints each item on a separate line.
+             }
+        } else {
+             System.out.println("String");
+             // everything
+         }
+
+
+
+    }
+
+}
---
+++ src/main/java/me/oliver/data/Data.java
@@ -0,0 +1,8 @@
+package me.oliver.data;
+
+public class Data {
+
+
+    public static final String[] array = new String[]{"Array Item #1", "Array Item #2"};
+
+}
