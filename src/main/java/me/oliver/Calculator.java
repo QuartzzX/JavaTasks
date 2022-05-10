dac1eee19bc51f2ba77d4efe507060b51b5a3169
QuartzzX <oliverlongnz@gmail.com>
2022-05-11 09:32:07 +1200

    Updates

--- src/main/java/me/oliver/Calculator.java
+++ src/main/java/me/oliver/Calculator.java
@@ -2,68 +2,107 @@ package me.oliver;
 
 import java.util.Scanner;
 
-    public class Calculator {
+public class Calculator {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-            System.out.println("Would you like to Add, Subtract, Divide, or Multiply?");
+        System.out.println("What operation would you like to use; Addition, Subtraction, Multiplication, Division, Powers, Floor-Division or Modulus?");
 
-            String mathType = scan.next();
+        String mathType = scan.next();
 
-            if (mathType.equalsIgnoreCase("add")) {
-                System.out.println("Whats the first number you want to add?");
+        if (mathType.equalsIgnoreCase("addition")) {
+            System.out.println("Whats the first number you want to add?");
 
-                int firstNumber = scan.nextInt();
+            int firstNumber = scan.nextInt();
 
-                System.out.println(firstNumber + " + ");
-                System.out.println("Enter your second number");
+            System.out.println("Enter your second number");
 
-                int secondNumber = scan.nextInt();
+            int secondNumber = scan.nextInt();
 
-                System.out.println(firstNumber + " + " + secondNumber + " =");
-                System.out.println(firstNumber + secondNumber);
+            System.out.println(firstNumber + " + " + secondNumber + " =");
+            System.out.println(firstNumber + secondNumber);
 
-            } else if (mathType.equalsIgnoreCase("subtract")) {
-                System.out.println("Whats the first number you want to subtract?");
+        }
+        else if (mathType.equalsIgnoreCase("subtraction")) {
+            System.out.println("Whats the first number you want to subtract?");
 
-                int firstNumber = scan.nextInt();
+            int firstNumber = scan.nextInt();
 
-                System.out.println(firstNumber + " - ");
-                System.out.println("Enter your second number");
+            System.out.println("Enter your second number");
 
-                int secondNumber = scan.nextInt();
+            int secondNumber = scan.nextInt();
 
-                System.out.println(firstNumber + " - " + secondNumber + " =");
-                System.out.println(firstNumber - secondNumber);
-            } else if (mathType.equalsIgnoreCase("divide")) {
-                System.out.println("Whats the first number you want to divide?");
+            System.out.println(firstNumber + " - " + secondNumber + " =");
+            System.out.println(firstNumber - secondNumber);
+        }
+        else if (mathType.equalsIgnoreCase("division")) {
+            System.out.println("Whats the first number you want to divide?");
 
-                int firstNumber = scan.nextInt();
+            int firstNumber = scan.nextInt();
 
-                System.out.println(firstNumber + " % ");
-                System.out.println("Enter your second number");
+            System.out.println("Enter your second number");
 
-                int secondNumber = scan.nextInt();
+            int secondNumber = scan.nextInt();
 
-                System.out.println(firstNumber + " % " + secondNumber + " =");
-                System.out.println(firstNumber / secondNumber);
-            } else if (mathType.equalsIgnoreCase("multiply")){
-                System.out.println("Whats the first number you want to multiply?");
+            System.out.println(firstNumber + " / " + secondNumber + " =");
+            System.out.println(firstNumber / secondNumber);
+        }
+        else if (mathType.equalsIgnoreCase("multiplication")) {
+            System.out.println("Whats the first number you want to multiply?");
 
-                int firstNumber = scan.nextInt();
+            int firstNumber = scan.nextInt();
 
-                System.out.println(firstNumber + " x ");
-                System.out.println("Enter your second number");
+            System.out.println("Enter your second number");
 
-                int secondNumber = scan.nextInt();
+            int secondNumber = scan.nextInt();
 
-                System.out.println(firstNumber + " x " + secondNumber + " =");
-                System.out.println(firstNumber * secondNumber);
-            }
+            System.out.println(firstNumber + " x " + secondNumber + " =");
+            System.out.println(firstNumber * secondNumber);
+        }
+        else if (mathType.equalsIgnoreCase("powers")) {
+            System.out.println("Whats is your base number");
 
+           int firstNumber = scan.nextInt();
+
+           System.out.println("Enter the exponent");
+
+           int secondNumber = scan.nextInt();
+
+            System.out.println(firstNumber + "^" + secondNumber + " =");
+            System.out.println(Math.pow(firstNumber, secondNumber));
+
+
+        }
+        else if (mathType.equalsIgnoreCase("modulus")) {
+            System.out.println("Please enter your first number");
+
+            int firstNumber = scan.nextInt();
+
+            System.out.println("Enter your second number");
+
+            int secondNumber = scan.nextInt();
+
+            System.out.println(firstNumber + " % " + secondNumber + " =");
+            System.out.println("Whole Number Equation = " + (firstNumber - (firstNumber % secondNumber) + "/" + (firstNumber - (firstNumber % secondNumber)) / secondNumber ));
+            System.out.println("Remainder = " + (firstNumber % secondNumber));
+
+        }
+        else if (mathType.equalsIgnoreCase("floor-division")){
+            System.out.println("Please enter your first number");
+
+            int firstNumber = scan.nextInt();
+
+            System.out.println("Please enter your second number");
+
+            int secondNumber = scan.nextInt();
+
+            System.out.println(firstNumber + " / " + secondNumber + " Floor =");
+            System.out.println(Math.floorDiv(firstNumber, secondNumber));
         }
 
     }
+
+}
 
