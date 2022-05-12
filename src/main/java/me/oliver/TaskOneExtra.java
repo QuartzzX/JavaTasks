df98825ab6fd632d41be42cc23130dc86c025057
QuartzzX <oliverlongnz@gmail.com>
2022-05-13 11:28:51 +1200

    Updates

---
+++ src/main/java/me/oliver/TaskOneExtra.java
@@ -0,0 +1,16 @@
+package me.oliver;
+// Task One Extra
+public class TaskOneExtra {
+
+    public static void main(String[] args) {
+        int[] array = {1, 3, 5, 7, 9};
+
+        for (int element : array) {
+            System.out.println(element);
+
+        } //end for loop
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+    }
+}
