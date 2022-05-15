0a5d43ef45cd98fe6c51508b696a6a49c9b5431a
QuartzzX <oliverlongnz@gmail.com>
2022-05-16 11:51:16 +1200

    Updates

--- src/main/java/me/oliver/TaskTwoExtra.java
+++ src/main/java/me/oliver/TaskTwoExtra.java
@@ -2,5 +2,15 @@ package me.oliver;
 //Task Two Extra
 //Write a Python program to append a new item to the end of the array.
 
+import java.util.Arrays;
+
 public class TaskTwoExtra {
+    public static void main(String[] args) {
+        int[] array = new int[] {3,6,9};
+        System.out.println(Arrays.toString(array));
+        array = Arrays.copyOf(array, array.length + 1); //create new array from old array and allocate one more element
+        array[array.length - 1] = 12;
+        System.out.println(Arrays.toString(array));
+    }
+
 }
