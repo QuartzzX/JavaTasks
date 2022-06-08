5b0ebf12dc3e7526dd80f4ac83fb5f46c283a4c5
QuartzzX <oliverlongnz@gmail.com>
2022-06-08 12:02:08 +1200

    Updates

---
+++ src/main/java/me/oliver/csv1.java
@@ -0,0 +1,17 @@
+package me.oliver;
+import java.io.*;
+import java.util.Scanner;
+public class csv1
+{
+    public static void main(String[] args) throws Exception
+    {
+//parsing a CSV file into Scanner class constructor
+        Scanner sc = new Scanner(new File("/Users/o.r.long/Library/CloudStorage/OneDrive-SaintKentigern/2022 - Year 12/Computer Science/Programming/CSV files/sample.csv"));
+        sc.useDelimiter(" , ");   //sets the delimiter pattern
+        while (sc.hasNext())  //returns a boolean value
+        {
+            System.out.print(sc.next());  //find and returns the next complete token from this scanner
+        }
+        sc.close();  //closes the scanner
+    }
+}
